af6fa47b3443be2b827035c8d3cc0aa2a1b0f4d1
gal <galinablokh0307@gmail.com>
2021-03-15 20:34:49 +0200

    requirements added

--- web_app.py
+++ web_app.py
@@ -11,6 +11,7 @@ import streamlit as st
 
 
 # Mean Pooling - Take attention mask into account for correct averaging
+@st.cache
 def mean_pooling(model_output, attention_mask):
     token_embeddings = model_output[0]  # First element of model_output contains all token embeddings
     input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
